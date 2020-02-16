8f185bd3fcd9605dea1afe750c5831a5fa13c1b7
Ahmed Ibrahim <a7mad3abed@gmail.com>
2020-02-16 23:27:06 +0100

    dataview successful

--- ListCtrlExmp/MyFrame.cpp
+++ ListCtrlExmp/MyFrame.cpp
@@ -1,11 +1,13 @@
 #include "MyFrame.h"
 #include "wx/listctrl.h"
 #include <vector>
+#include "wx/dataview.h"
+#include <string>
 
 class Item
 {
 public:
-    const char * id = "";
+    int id = 0;
     const char* name = "" ;
 
 };
@@ -13,6 +15,36 @@ public:
 MyFrame::MyFrame(const wxString& title)
     :wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxDefaultSize, wxDEFAULT_FRAME_STYLE)
 {
+    std::vector<Item> items;
+    items.push_back({ 0, "Ahmed" });
+    items.push_back({ 1, "Lobna" });
+    items.push_back({ 2, "Mohamed" });
+    items.push_back({ 3, "Khadiga" });
+
+    wxDataViewListCtrl* list = new wxDataViewListCtrl(this, wxID_ANY);
+    list->AppendTextColumn("id");
+    list->AppendTextColumn("name");
+
+    wxVector<wxVariant> data;
+
+    data.push_back(wxVariant(std::to_string(items[0].id)));
+    data.push_back(wxVariant(items[0].name));
+
+    list->AppendItem(data);
+
+    data.clear();
+    data.push_back(wxVariant(std::to_string(items[1].id)));
+    data.push_back(wxVariant(items[1].name));
+
+    list->AppendItem(data);
+
+    data.clear();
+    data.push_back(wxVariant(std::to_string(items[2].id)));
+    data.push_back(wxVariant(items[2].name));
+
+    list->AppendItem(data);
+
+    /*
     std::vector<Item> items;
     items.push_back({ "0", "Ahmed" });
     items.push_back({ "1", "Lobna" });
@@ -24,6 +56,7 @@ MyFrame::MyFrame(const wxString& title)
     col1.SetId(0);
     col1.SetText("id");
     col1.SetWidth(40);
+    col1.SetBackgroundColour(wxColor(*wxRED));
     list->InsertColumn(0,col1);
 
     wxListItem col2;
@@ -46,4 +79,7 @@ MyFrame::MyFrame(const wxString& title)
     {
         list->SetItem(i, 1, items[i].name);
     }
+    */
+
+
 }
